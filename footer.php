828173bd1ba48ee283b4fe34609b36cf909c4b7c
darcy w. christ <darcy@1000camels.com>
2013-02-28 14:27:02 +0800

    initial commit of theme

---
+++ footer.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains footer content and the closing of the
+ * #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+	</div><!-- #main .wrapper -->
+	<footer id="colophon" role="contentinfo">
+		<div class="site-info">
+			<span>Sponsored by</span><br/>
+			<a href="http://www.lordwilson-heritagetrust.org.hk/"><img src="http://www.docomomo.hk/wp-content/uploads/2013/02/lord-wilson-logo-300x136.png" alt="Lord Wilson Heritage Trust logo" /></a>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
